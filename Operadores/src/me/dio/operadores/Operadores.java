4227eeb57fb3f14c805bbb2317d0ec26d6270f15
Tiago Felipe <tiagosantos0412@gmail.com>
2023-01-05 17:57:44 -0300

    Adicionando o pacote no projeto Operadores

--- Operadores/src/me/dio/operadores/Operadores.java
+++ Operadores/src/me/dio/operadores/Operadores.java
@@ -1,5 +1,7 @@
+package me.dio.operadores;
+
 public class Operadores {
     public static void main(String[] args) throws Exception {
-        System.out.println("Chama nos operadores");
+        System.out.println("Chama nos operadores com pacote");
     }
 }
